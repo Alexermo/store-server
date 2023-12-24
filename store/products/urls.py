8b494a8f1f415ab319a42c8b6959b1f791516865
Alexermo <89266703634@mail.ru>
2023-12-24 23:19:31 +0300

    basket_update

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import index, products
+from .views import index, products, basket_add, basket_delete
 
 app_name = 'products'
 
 urlpatterns = [
     path('', index, name='index'),
     path('products/', products, name='products'),
+    path('products/basket/<int:product_id>/', basket_add, name='basket_add'),
+    path('products/basket_delete/<int:basket_id>/',
+         basket_delete, name='basket_delete'),
 ]
--- store/users/views.py
+++ store/users/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
 
-from .models import User
+from django.contrib.auth.decorators import login_required
+
 from .form import UserLoginForm, UserRegistrationForm, UserProfileForm
+
+from products.models import Basket
 
 
 def login(request):
@@ -36,6 +39,7 @@ def register(request):
     return render(request, "users/register.html", context)
 
 
+@login_required
 def profile(request):
     if request.method == "POST":
         form = UserProfileForm(instance=request.user,
@@ -45,7 +49,10 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'form': form}
+
+    context = {
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user)}
     return render(request, 'users/profile.html', context)
 
 
