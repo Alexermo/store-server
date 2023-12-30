790b65e7abefc7b297d518ce8ca1ebd1bb199ddd
Alexermo <89266703634@mail.ru>
2023-12-30 12:54:35 +0300

    admin_panel

--- store/products/urls.py
+++ store/products/urls.py
@@ -7,6 +7,8 @@ app_name = 'products'
 urlpatterns = [
     path('', index, name='index'),
     path('products/', products, name='products'),
+    path('page/<int:page_number>', products, name='paginator'),
+    path('products/category/<int:category_id>/', products, name='category'),
     path('products/basket/<int:product_id>/', basket_add, name='basket_add'),
     path('products/basket_delete/<int:basket_id>/',
          basket_delete, name='basket_delete'),
