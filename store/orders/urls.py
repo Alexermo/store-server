2ab3ba1ccd7409d9742d7afdabb340a22cc181ef
Alexermo <89266703634@mail.ru>
2024-02-01 18:51:24 +0300

    tune stripe webhook

--- store/orders/urls.py
+++ store/orders/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-
-from .views import OrderCreateView
+from .views import OrderCancelView, OrderCreateView, OrderSuccessView
 
 app_name = 'orders'
 
 urlpatterns = [
     path('order-create/', OrderCreateView.as_view(), name='order_create'),
+    path('order-success/', OrderSuccessView.as_view(), name='order_success'),
+    path('order-canceled/', OrderCancelView.as_view(), name='order_canceled'),
 
 ]
--- store/products/admin.py
+++ store/products/admin.py
@@ -9,7 +9,7 @@ admin.site.register(ProductCategory)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'quantity', 'category')
     fields = ('name', 'description',
-              ('price', 'quantity',), 'category', 'image')
+              ('price', 'quantity',), 'stripe_product_price_id', 'category', 'image')
     readonly_fields = ('description',)
     search_fields = ('name',)
     ordering = ('name',)
